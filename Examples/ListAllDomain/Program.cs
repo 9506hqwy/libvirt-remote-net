e9c5470d70eb25a7dfda22ec74bffe92ef248d44
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2023-01-19 20:57:10 +0900

    サンプルプログラムを追加した。

---
+++ Examples/ListAllDomain/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Sockets;
+using LibvirtRemote;
+using Xdr;
+
+try
+{
+    await Work(args);
+}
+catch (Exception e)
+{
+    await Console.Error.WriteLineAsync(string.Format("{0}", e));
+}
+
+async Task Work(string[] args)
+{
+    using var socket = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified);
+    await socket.ConnectAsync(new UnixDomainSocketEndPoint("/var/run/libvirt/libvirt-sock"));
+
+    using var stream = new NetworkStream(socket);
+
+    using var client = new VirtClient(stream);
+
+    await client.ConnectOpenAsync(new XdrOption<string>("qemu:///system"), 0, default);
+
+    (var domains, var _) = await client.ConnectListAllDomainsAsync(1, 1 | 2, default);
+    foreach (var domain in domains)
+    {
+        await Console.Out.WriteLineAsync(domain.Name);
+    }
+
+    await client.ConnectCloseAsync(default);
+
+    await socket.DisconnectAsync(false);
+}
---
+++ Examples/PollDomainEvent/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Sockets;
+using Binding;
+using LibvirtRemote;
+using Xdr;
+
+try
+{
+    await Work(args);
+}
+catch (Exception e)
+{
+    await Console.Error.WriteLineAsync(string.Format("{0}", e));
+}
+
+async Task Work(string[] args)
+{
+    using var tcp = new TcpClient("127.0.0.1", 16509);
+
+    using var stream = tcp.GetStream();
+
+    using var client = new VirtClient(stream);
+
+    await client.ConnectOpenAsync(new XdrOption<string>("qemu:///system"), 0, default);
+
+    var callbackId = await client.ConnectDomainEventCallbackRegisterAnyAsync(0, null, default);
+    var eventStream = client.GetEventStream(callbackId);
+
+    var e = await eventStream.ReadAsync(default);
+    if (e is RemoteDomainEventCallbackLifecycleMsg m)
+    {
+        await Console.Out.WriteLineAsync(string.Format("{0} event={1} detail={2}", m.Msg.Dom.Name, m.Msg.Event, m.Msg.Detail));
+    }
+
+    client.DeleteEventStream(eventStream);
+
+    await client.ConnectDomainEventCallbackDeregisterAnyAsync(callbackId, default);
+
+    await client.ConnectCloseAsync(default);
+}
