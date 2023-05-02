8ec2bcfab0ed0b100b6697e6e44544844d583933
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2023-05-03 08:29:10 +0900

    自動生成ファイルを v9.3.0 に更新した。

--- Tool/gen.sh
+++ Tool/gen.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-VERSION="v9.2.0"
+VERSION="v9.3.0"
 PROTO_URL="https://raw.githubusercontent.com/libvirt/libvirt/${VERSION}/src/rpc/virnetprotocol.x"
 LXC_URL="https://raw.githubusercontent.com/libvirt/libvirt/${VERSION}/src/remote/lxc_protocol.x"
 QEMU_URL="https://raw.githubusercontent.com/libvirt/libvirt/${VERSION}/src/remote/qemu_protocol.x"
