cf332be172d9f98cde56d9dcb502d4fc0bf66623
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2024-10-01 21:23:17 +0900

    自動生成ファイルを v10.8.0 に更新した。

--- Tool/gen.sh
+++ Tool/gen.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-VERSION="v10.7.0"
+VERSION="v10.8.0"
 PROTO_URL="https://raw.githubusercontent.com/libvirt/libvirt/${VERSION}/src/rpc/virnetprotocol.x"
 LXC_URL="https://raw.githubusercontent.com/libvirt/libvirt/${VERSION}/src/remote/lxc_protocol.x"
 QEMU_URL="https://raw.githubusercontent.com/libvirt/libvirt/${VERSION}/src/remote/qemu_protocol.x"
