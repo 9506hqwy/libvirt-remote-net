721af402c96759ef80f2f79c3ce546c27559c193
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2025-11-03 22:55:05 +0900

    自動生成ファイルを v11.9.0 に更新した。

--- Tool/gen.sh
+++ Tool/gen.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-VERSION="v11.8.0"
+VERSION="v11.9.0"
 PROTO_URL="https://raw.githubusercontent.com/libvirt/libvirt/${VERSION}/src/rpc/virnetprotocol.x"
 LXC_URL="https://raw.githubusercontent.com/libvirt/libvirt/${VERSION}/src/remote/lxc_protocol.x"
 QEMU_URL="https://raw.githubusercontent.com/libvirt/libvirt/${VERSION}/src/remote/qemu_protocol.x"
